afd75311c1a6a299273d7bc68829e9c01c7b713b
Landry Irakoze <irakozel@gmail.com>
2019-09-10 16:43:46 -0400

    added sales table

--- data/migrations/20190910152659_bootstrap.js
+++ data/migrations/20190910152659_bootstrap.js
@@ -19,9 +19,23 @@ exports.up = function(knex) {
         tbl.string('transmission', 128);
         tbl.string('titleStatus', 128);
     })
+    .createTable('sales', tbl => {
+        tbl.increments();
+        tbl.integer('Car_Id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('cars');
+        tbl.integer('price')
+            .unsigned()
+            .notNullable();
+        tbl.boolean('sold')
+            .notNullable();
+    })
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists('sales')
     .dropTableIfExists('cars')
 };
